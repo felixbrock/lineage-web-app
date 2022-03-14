5ab58263ce4c19438610895829f213eeb19e52b4
Felix Brockmeier <felix.brockmeier@outlook.com>
2022-03-14 18:30:40 +0100

    first working data lineage graph

--- src/pages/lineage/lineage.tsx
+++ src/pages/lineage/lineage.tsx
@@ -1,200 +1,62 @@
 import React, { ReactElement, useEffect, useState } from 'react';
 
-import G6, { Graph} from '@antv/g6';
+import G6, { Graph, GraphData, INode } from '@antv/g6';
 import { Lineage } from './lineage-items';
 
 export default (): ReactElement => {
-  // const data = {
-  //   label: 'Modeling Methods',
-  //   children: [
-  //     {
-  //       label: 'Classification',
-  //       children: [
-  //         {
-  //           label: 'Logistic regression',
-  //         },
-  //         {
-  //           label: 'Linear discriminant analysis',
-  //         },
-  //         {
-  //           label: 'Rules',
-  //         },
-  //         {
-  //           label: 'Decision trees',
-  //         },
-  //         {
-  //           label: 'Naive Bayes',
-  //         },
-  //         {
-  //           label: 'K nearest neighbor',
-  //         },
-  //         {
-  //           label: 'Probabilistic neural network',
-  //         },
-  //         {
-  //           label: 'Support vector machine',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       label: 'Consensus',
-  //       children: [
-  //         {
-  //           label: 'Models diversity',
-  //           children: [
-  //             {
-  //               label: 'Different initializations',
-  //             },
-  //             {
-  //               label: 'Different parameter choices',
-  //             },
-  //             {
-  //               label: 'Different architectures',
-  //             },
-  //             {
-  //               label: 'Different modeling methods',
-  //             },
-  //             {
-  //               label: 'Different training sets',
-  //             },
-  //             {
-  //               label: 'Different feature sets',
-  //             },
-  //           ],
-  //         },
-  //         {
-  //           label: 'Methods',
-  //           children: [
-  //             {
-  //               label: 'Classifier selection',
-  //             },
-  //             {
-  //               label: 'Classifier fusion',
-  //             },
-  //           ],
-  //         },
-  //         {
-  //           label: 'Common',
-  //           children: [
-  //             {
-  //               label: 'Bagging',
-  //             },
-  //             {
-  //               label: 'Boosting',
-  //             },
-  //             {
-  //               label: 'AdaBoost',
-  //             },
-  //           ],
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       label: 'Regression',
-  //       children: [
-  //         {
-  //           label: 'Multiple linear regression',
-  //         },
-  //         {
-  //           label: 'Partial least squares',
-  //         },
-  //         {
-  //           label: 'Multi-layer feedforward neural network',
-  //         },
-  //         {
-  //           label: 'General regression neural network',
-  //         },
-  //         {
-  //           label: 'Support vector regression',
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // };
-
-  const data = {
+  const data: GraphData = {
     nodes: [
-      {
-        id: '0',
-        label: '0',
-      },
-      {
-        id: '1',
-        label: '1',
-      },
-      {
-        id: '2',
-        label: '2',
-      },
-      {
-        id: '3',
-        label: '3',
-      },
-      {
-        id: '4',
-        label: '4',
-      },
-      {
-        id: '5',
-        label: '5',
-      },
-      {
-        id: '6',
-        label: '6',
-      },
-      {
-        id: '7',
-        label: '7',
-      },
-      {
-        id: '8',
-        label: '8',
-      },
-      {
-        id: '9',
-        label: '9',
-      },
+      { id: '0', label: '0', comboId: 'a' },
+      { id: '1', label: '1', comboId: 'a' },
+      { id: '2', label: '2', comboId: 'a' },
+      { id: '3', label: '3', comboId: 'b' },
+      { id: '4', label: '4', comboId: 'b' },
+      { id: '5', label: '5', comboId: 'b' },
+      { id: '6', label: '6', comboId: 'b' },
+      { id: '7', label: '7', comboId: 'c' },
+      { id: '8', label: '8', comboId: 'c' },
+      { id: '9', label: '9', comboId: 'c' },
+      { id: '10', label: '10', comboId: 'd' },
+      { id: '11', label: '11', comboId: 'd' },
+      { id: '12', label: '12', comboId: 'd' },
+      { id: '13', label: '13', comboId: 'd' },
+      { id: '14', label: '14', comboId: 'e' },
+      { id: '15', label: '15', comboId: 'e' },
+      { id: '16', label: '16', comboId: 'e' },
+      { id: '17', label: '17', comboId: 'f' },
+      { id: '18', label: '18', comboId: 'f' },
+      { id: '19', label: '19', comboId: 'f' },
+      { id: '20', label: '20', comboId: 'f' },
+      { id: '21', label: '21', comboId: 'f' },
     ],
     edges: [
-      {
-        source: '0',
-        target: '1',
-      },
-      {
-        source: '0',
-        target: '2',
-      },
-      {
-        source: '1',
-        target: '4',
-      },
       {
         source: '0',
         target: '3',
       },
       {
-        source: '3',
+        source: '0',
         target: '4',
       },
       {
-        source: '4',
-        target: '5',
-      },
-      {
-        source: '4',
-        target: '6',
-      },
-      {
-        source: '5',
+        source: '0',
         target: '7',
       },
       {
-        source: '5',
-        target: '8',
+        source: '1',
+        target: '5',
       },
       {
-        source: '8',
-        target: '9',
+        source: '1',
+        target: '6',
+      },
+      {
+        source: '2',
+        target: '5',
+      },
+      {
+        source: '2',
+        target: '8',
       },
       {
         source: '2',
@@ -202,7 +64,61 @@ export default (): ReactElement => {
       },
       {
         source: '3',
-        target: '9',
+        target: '21',
+      },
+      {
+        source: '3',
+        target: '11',
+      },
+      {
+        source: '3',
+        target: '12',
+      },
+      {
+        source: '5',
+        target: '10',
+      },
+      {
+        source: '6',
+        target: '14',
+      },
+      {
+        source: '7',
+        target: '15',
+      },
+      {
+        source: '7',
+        target: '17',
+      },
+      {
+        source: '8',
+        target: '20',
+      },
+    ],
+    combos: [
+      {
+        id: 'a',
+        label: 'a',
+      },
+      {
+        id: 'b',
+        label: 'b',
+      },
+      {
+        id: 'c',
+        label: 'c',
+      },
+      {
+        id: 'd',
+        label: 'd',
+      },
+      {
+        id: 'e',
+        label: 'e',
+      },
+      {
+        id: 'f',
+        label: 'f',
       },
     ],
   };
@@ -212,164 +128,43 @@ export default (): ReactElement => {
   useEffect(() => {
     if (graph) return;
 
-    //   G6.registerNode(
-    //     'tree-node',
-    //     {
-    //       drawLabel: (cfg: ModelConfig, group: IGroup) => {
-    //         const text = group.addShape('text', {
-    //           attrs: {
-    //           },
-    //           name: 'text-shape',
-    //         });
-
-    //         return text;
-    //       },
-
-    //       drawShape: function drawShape(cfg, group) {
-    //         if (!group) throw new ReferenceError('Shape group not found');
-    //         if (!cfg) throw new ReferenceError('Model config not found');
-    //         const rect = group.addShape('rect', {
-    //           attrs: {
-    //             fill: '#fff',
-    //             stroke: '#666',
-    //             x: 0,
-    //             y: 0,
-    //             width: 1,
-    //             height: 1,
-    //           },
-    //           name: 'rect-shape',
-    //         });
-    //         // if(!cfg.label) throw new ReferenceError('No label found for node');
-    //         // if(typeof cfg.label === 'string') throw new ReferenceError('No label found for node');
-    //         const content = cfg.label;
-    //         const text = group.addShape('text', {
-    //           attrs: {
-    //             text: content,
-    //             x: 0,
-    //             y: 0,
-    //             textAlign: 'left',
-    //             textBaseline: 'middle',
-    //             fill: '#666',
-    //           },
-    //           name: 'text-shape',
-    //         });
-    //         const bbox = text.getBBox();
-    //         rect.attr({
-    //           x: -bbox.width / 2 - 4,
-    //           y: -bbox.height / 2 - 6,
-    //           width: bbox.width + 12,
-    //           height: bbox.height + 12,
-    //         });
-    //         text.attr({
-    //           x: -bbox.width / 2,
-    //           y: 0,
-    //         });
-
-    //         return rect;
-    //       },
-    //       update: (cfg, item) => {
-    //         const group = item.getContainer();
-    //         const icon = group.find((e) => e.get('name') === 'collapse-icon');
-    //         icon.attr(
-    //           'symbol',
-    //           cfg.collapsed ? G6.Marker.expand : G6.Marker.collapse
-    //         );
-    //       },
-    //     },
-    //     'single-node'
-    //   );
-
     const container = document.getElementById('lineage');
     if (!container) throw new ReferenceError(`Container for graph not found`);
-    const width = container.scrollWidth;
-    const height = container.scrollHeight || 500;
-    //   const graphObj = new G6.TreeGraph({
-    //     container: container,
-    //     width,
-    //     height,
-    //     modes: {
-    //       default: [
-    //         {
-    //           type: 'collapse-expand',
-    //           onChange: function onChange(item, collapsed) {
-    //             if (!item) throw new ReferenceError('onchange item missing');
-    //             const dataObj = item.get('model');
-    //             graphObj.updateItem(item, {
-    //               collapsed,
-    //             });
-    //             dataObj.collapsed = collapsed;
-    //             return true;
-    //           },
-    //         },
-    //         'drag-canvas',
-    //         'zoom-canvas',
-    //       ],
-    //     },
-    //     defaultNode: {
-    //       type: 'tree-node',
-    //       anchorPoints: [
-    //         [0, 0.5],
-    //         [1, 0.5],
-    //       ],
-    //     },
-    //     defaultEdge: {
-    //       type: 'cubic-horizontal',
-    //       style: {
-    //         stroke: '#A3B1BF',
-    //       },
-    //     },
-    //     layout: {
-    //       type: 'compactBox',
-    //       direction: 'LR',
-    //       getId: function getId(d: any) {
-    //         return d.id;
-    //       },
-    //       getHeight: function getHeight() {
-    //         return 16;
-    //       },
-    //       getWidth: function getWidth() {
-    //         return 16;
-    //       },
-    //       getVGap: function getVGap() {
-    //         return 20;
-    //       },
-    //       getHGap: function getHGap() {
-    //         return 80;
-    //       },
-    //     },
-    //   });
-
-    //   if (typeof window !== 'undefined')
-    //     window.onresize = () => {
-    //       if (!graphObj || graphObj.get('destroyed')) return;
-    //       if (!container || !container.scrollWidth || !container.scrollHeight)
-    //         return;
-    //       graphObj.changeSize(container.scrollWidth, container.scrollHeight);
-    //     };
+    const width = container.getBoundingClientRect().width;
+    const height = container.getBoundingClientRect().height || 500;
 
     const graphObj = new G6.Graph({
       container,
       width,
       height,
       fitView: true,
+      fitViewPadding: 30,
+      animate: true,
+      groupByTypes: false,
       modes: {
-        default: ['drag-canvas', 'zoom-canvas'],
+        default: ['drag-canvas', 'zoom-canvas', 'click-select'],
       },
       layout: {
         type: 'dagre',
         rankdir: 'LR',
-        align: 'UL',
+        sortByCombo: true,
         controlPoints: true,
-        nodesepFunc: () => 1,
-        ranksepFunc: () => 1,
+        nodesep: 0,
       },
       defaultNode: {
-        size: [30, 20],
+        // size: [30, 20],
         type: 'rect',
         style: {
-          lineWidth: 2,
+          lineWidth: 1,
           stroke: '#5B8FF9',
           fill: '#C6E5FF',
+        },
+      },
+      nodeStateStyles: {
+        selected: {
+          stroke: '#666',
+          lineWidth: 2,
+          fill: 'steelblue',
         },
       },
       defaultEdge: {
@@ -379,9 +174,22 @@ export default (): ReactElement => {
         style: {
           endArrow: {
             path: 'M 0,0 L 8,4 L 8,-4 Z',
-            fill: '#e2e2e2',
+            fill: '#ffffff',
           },
-          radius: 20,
+          radius: 50,
+        },
+      },
+      edgeStateStyles: {
+        selected: {
+          style: {
+            stroke: '#eb4034',
+          }
+        }
+      },
+      defaultCombo: {
+        type: 'rect',
+        style: {
+          fillOpacity: 0.1,
         },
       },
     });
@@ -393,6 +201,31 @@ export default (): ReactElement => {
           return;
         graphObj.changeSize(container.scrollWidth, container.scrollHeight);
       };
+
+    graphObj.on('node:click', (event) => {
+      if (!event.item)
+        throw new ReferenceError('Clicked node could not be referenced');
+      console.log(event.item.getID());
+      console.log(event);
+
+      const isNode = (object: any): object is INode => 'getEdges' in object;
+
+      if (!isNode(event.item))
+        throw new ReferenceError('Node does not have getEdges member');
+
+      const selectedEdges = graphObj.findAllByState('edge', 'selected');
+      selectedEdges.forEach(edge => edge.clearStates());
+
+      console.log(selectedEdges);
+      
+      event.item.getEdges().forEach((edge) => {
+       graphObj.setItemState(edge.getID(), 'selected', true); 
+      });
+    });
+
+    graphObj.on('nodeselectchange', (event) => {
+      if(!event.select) console.log(event);
+    });
 
     graphObj.data(data);
     graphObj.render();

4c4e5e20703aef4fbd7f49a535fa78ebc0617dc9
Felix Brockmeier <felix.brockmeier@outlook.com>
2022-06-22 11:42:16 +0200

    test

--- src/pages/installation/github.tsx
+++ src/pages/installation/github.tsx
@@ -5,10 +5,12 @@ import { useSearchParams } from 'react-router-dom';
 export default (): ReactElement => {
   const [searchParams] = useSearchParams();
   
-  const code = searchParams.get('code');
+  const code = searchParams.get('test');
+  console.log(searchParams.get('test'));
   console.log(searchParams.get('code'));
   console.log(searchParams, 'params');
   
+  // todo - find workaround for rerouting bug in case of code query param
   
 
   return <>

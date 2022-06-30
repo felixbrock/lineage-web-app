effac77be61e19b5f1c4ee0ce3b47115f1711fd1
Dinesh-CitoData <107985304+Dinesh-CitoData@users.noreply.github.com>
2022-06-30 19:58:07 +0100

    Changed text

--- src/components/integration.tsx
+++ src/components/integration.tsx
@@ -15,7 +15,7 @@ export default (index: number) => {
       
       //href: 'https://google.com',
       //href: 'https://slack.com/oauth/v2/authorize?client_id=3743097674080.3733004026065&scope=app_mentions:read,channels:history,channels:join,channels:manage,channels:read,chat:write,chat:write.public,conversations.connect:write,files:read,files:write,groups:read,groups:write,im:read,im:write,links:read,mpim:read,mpim:write,users:read,users:write',
-      label: 'Install Cito Slack App',
+      label: 'Install to Slack Workspace',
     },
     3: {
       href: 'https://google.com',

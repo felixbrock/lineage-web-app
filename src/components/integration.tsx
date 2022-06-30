89b31764a3e456f14e8b72a90c65a2720fce4997
Dinesh-CitoData <107985304+Dinesh-CitoData@users.noreply.github.com>
2022-06-30 19:56:56 +0100

    changed slack url

--- src/components/integration.tsx
+++ src/components/integration.tsx
@@ -11,7 +11,9 @@ export default (index: number) => {
       label: 'Google',
     },
     2: {
-      href: 'https://google.com',
+      href: 'https://slack.com/oauth/v2/authorize?client_id=3743837702852.3728367364791&scope=channels:history,channels:read,chat:write,chat:write.public,channels:manage,files:read,channels:join,files:write,users:read,im:read,users:write,im:write,users.profile:read,conversations.connect:write,im:history,links:read,links:write,mpim:read,mpim:write&user_scope=',
+      
+      //href: 'https://google.com',
       //href: 'https://slack.com/oauth/v2/authorize?client_id=3743097674080.3733004026065&scope=app_mentions:read,channels:history,channels:join,channels:manage,channels:read,chat:write,chat:write.public,conversations.connect:write,files:read,files:write,groups:read,groups:write,im:read,im:write,links:read,mpim:read,mpim:write,users:read,users:write',
       label: 'Install Cito Slack App',
     },

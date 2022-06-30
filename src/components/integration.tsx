fba4e9c9c61f9766bcb3190df209cf5280c8ff83
Dinesh-CitoData <dinesh@citodata.com>
2022-06-30 15:34:55 +0100

    Checking if add slack to worksapce works

--- src/components/integration.tsx
+++ src/components/integration.tsx
@@ -11,7 +11,8 @@ export default (index: number) => {
       label: 'Google',
     },
     2: {
-      href: 'https://slack.com/oauth/v2/authorize?client_id=3743097674080.3733004026065&scope=app_mentions:read,channels:history,channels:join,channels:manage,channels:read,chat:write,chat:write.public,conversations.connect:write,files:read,files:write,groups:read,groups:write,im:read,im:write,links:read,mpim:read,mpim:write,users:read,users:write',
+      href: 'https://google.com',
+      //href: 'https://slack.com/oauth/v2/authorize?client_id=3743097674080.3733004026065&scope=app_mentions:read,channels:history,channels:join,channels:manage,channels:read,chat:write,chat:write.public,conversations.connect:write,files:read,files:write,groups:read,groups:write,im:read,im:write,links:read,mpim:read,mpim:write,users:read,users:write',
       label: 'Install Cito Slack App',
     },
     3: {

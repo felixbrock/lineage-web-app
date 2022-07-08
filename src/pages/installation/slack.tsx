93ecc5380a4b57c28e505936d01f489a12262663
Dinesh-CitoData <107985304+Dinesh-CitoData@users.noreply.github.com>
2022-07-08 18:11:08 +0100

    Update slack.tsx

--- src/pages/installation/slack.tsx
+++ src/pages/installation/slack.tsx
@@ -8,8 +8,19 @@ export default (): ReactElement => {
   const {code} = useParams();
   console.log(searchParams.get('code'));
   console.log(code);
+  
+  const url = "https://slack.com/api/oauth.v2.access?client_id=3743837702852.3728367364791&client_secret=bd2a44af39cb4eb6bed61f3aa4b50162&code="+code;
+  const response = fetch(url, {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json'}
+  });
+  
+  console.log("Response",response);
+  
   return <>
   <p>something</p>
-  {code }</>;
+  {code }
+  {response}</>;
 };
 

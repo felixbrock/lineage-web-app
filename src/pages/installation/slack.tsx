acaf7bb93b7737b9804a805ffcb3c948754b4caa
Dinesh-CitoData <107985304+Dinesh-CitoData@users.noreply.github.com>
2022-07-13 14:13:22 +0100

    Update slack.tsx

--- src/pages/installation/slack.tsx
+++ src/pages/installation/slack.tsx
@@ -3,25 +3,47 @@ import { useSearchParams,useParams } from 'react-router-dom';
 
 
 export default (): ReactElement => {
-  console.log("code");
-  const [searchParams] = useSearchParams();
-  const {code} = useParams();
-  console.log(searchParams.get('code'));
-  console.log(code);
+//   console.log("code");
+//   const [searchParams] = useSearchParams();
+//   const {code} = useParams();
+//   console.log(searchParams.get('code'));
+//   console.log(code);
   
-  const url = "https://slack.com/api/oauth.v2.access?client_id=3743837702852.3728367364791&client_secret=bd2a44af39cb4eb6bed61f3aa4b50162&code="+code;
-  const response = fetch(url, {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded'}
+//   const url = "https://slack.com/api/oauth.v2.access?client_id=3743837702852.3728367364791&client_secret=bd2a44af39cb4eb6bed61f3aa4b50162&code="+code;
+//   const response = fetch(url, {
+//     method: 'POST',
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/x-www-form-urlencoded'}
+//   });
+  
+//   console.log("Response",response);
+  
+  var axios = require('axios');
+
+  var config = {
+    method: 'post',
+    url: 'https://slack.com/api/oauth.v2.access?client_id=3743837702852.3728367364791&client_secret=1fec399e9b4df09534572c709ad86fec&code=3334524827045.3793902891555.bfea9a43327dac26a309cf3148ce904caec850eee68ca1f2ed14a3afcdd5f2a9',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded'
+      // 'Cookie': 'b=2a41a35826c0f18b93ff25842351fc71; x=2a41a35826c0f18b93ff25842351fc71.1657713965'
+    }
+  };
+
+  axios(config)
+  .then(function (response: { data: any }) {
+    // console.log(JSON.stringify(response.data));
+    var res_data = response.data;
+    console.log("\n\nAccess Token: ",res_data["access_token"]);
+    // get_token(res_data);
+  })
+  .catch(function (error: any) {
+    console.log(error);
   });
-  
-  console.log("Response",response);
+
   
   return <>
   <p>something</p>
-  {code }
-  {response}</>;
+  </>;
 };
 

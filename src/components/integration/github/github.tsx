ce63e8a0aac77126591a7e349f1df840e295684e
nasir <nm219@ic.ac.uk>
2022-08-16 17:05:46 +0100

    added code for gihub redirect and added new integration endpoints

--- src/components/integration/github/github.tsx
+++ src/components/integration/github/github.tsx
@@ -1,3 +1,8 @@
+import { Button } from '@mui/material';
 import { ReactElement } from 'react';
 
-export default ():ReactElement => (<>'https://github.com/apps/cito-data/installations/new'</>);
+export default ():ReactElement => (
+<a href='https://github.com/apps/cito-data/installations/new' target="_blank">
+  <Button> Install Cito Data Github App </Button>
+</a>
+);

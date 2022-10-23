b160475c0600ca806df3124a4d3673edf69e96d1
Philip Bizimis <bizimisphilip@gmail.com>
2022-10-24 00:01:32 +0200

    Handle different github integration states

--- src/components/integration/github/github.tsx
+++ src/components/integration/github/github.tsx
@@ -1,17 +1,14 @@
-import {
-  Button,
-  List,
-  ListItem,
-  ListItemText,
-  Divider,
-  ListItemIcon,
-} from '@mui/material';
+import { List, ListItem, ListItemText, ListItemIcon } from '@mui/material';
 import { ReactElement, useEffect, useState } from 'react';
 import GithubApiRepo from '../../../infrastructure/github-api/github-api-repo';
 import IntegrationApiRepo from '../../../infrastructure/integration-api/integration-api-repo';
 import './github.scss';
 import { BiGitBranch } from 'react-icons/bi';
 import { mode } from '../../../config';
+import {
+  ButtonBig,
+  ButtonSmall,
+} from '../../../pages/lineage/components/buttons';
 
 interface GithubProps {
   installationId?: string;
@@ -35,6 +32,7 @@ export default ({
     repoNames: [],
     checked: false,
   });
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (installationId && accessToken) {
@@ -44,6 +42,7 @@ export default ({
             repoNames: repos.map((repo) => repo.full_name),
             checked: true,
           });
+          setIsLoading(false);
         })
         .catch((error: any) => {
           console.trace(error);
@@ -59,6 +58,7 @@ export default ({
               repoNames: profile.repositoryNames,
               checked: true,
             });
+          setIsLoading(false);
         })
         .catch((error: any) => {
           console.trace(error);
@@ -68,6 +68,7 @@ export default ({
 
   useEffect(() => {
     if (!repoNameResult.checked) return;
+    setIsLoading(false);
 
     if (installationId && accessToken)
       IntegrationApiRepo.getGithubProfile(
@@ -96,39 +97,75 @@ export default ({
 
   return (
     <>
-      <h4>Connect to Github</h4>
-      <Divider />
+      {isLoading && (
+        <div className="flex w-full items-center justify-center">
+          <svg
+            className="h-10 w-10 animate-spin text-cito"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      )}
+      {!isLoading && repoNameResult.repoNames.length === 0 && (
+        <div className="flex w-full items-center justify-center">
+          <a
+            href={`https://github.com/apps/${
+              mode === 'development' ? 'cito-data-dev' : 'cito-data'
+            }/installations/new?state=${organizationId}`}
+          >
+            <ButtonBig
+              buttonText="Install the CitoData Github App"
+              onClick={() => {}}
+              className="bg-cito text-gray-50 hover:bg-indigo-600"
+            />
+          </a>
+        </div>
+      )}
+      {!isLoading && repoNameResult.repoNames.length > 0 && (
+        <>
+          <div className="flex w-full items-center justify-between">
+            <p className="caption">Installed on Following Repositories:</p>
+            <a
+              href={`https://github.com/apps/${
+                mode === 'development' ? 'cito-data-dev' : 'cito-data'
+              }/installations/new?state=${organizationId}`}
+            >
+              <ButtonSmall
+                buttonText="Configure Repositories"
+                onClick={() => {}}
+              />
+            </a>
+          </div>
 
-      <Button
-        sx={{
-          minHeight: 0,
-          minWidth: 0,
-          padding: 0,
-          mt: 2,
-          mb: 2,
-          fontWeight: 'bold',
-        }}
-        href={`https://github.com/apps/${
-          mode === 'development' ? 'cito-data-dev' : 'cito-data'
-        }/installations/new?state=${organizationId}`}
-      >
-        Install Cito Data Github App
-      </Button>
-
-      <p className="caption">Installed on Following Repositories:</p>
-
-      <List>
-        {repoNameResult.repoNames.map((name) => {
-          return (
-            <ListItem dense={true}>
-              <ListItemIcon>
-                <BiGitBranch />
-              </ListItemIcon>
-              <ListItemText primary={name} />
-            </ListItem>
-          );
-        })}
-      </List>
+          <List>
+            {repoNameResult.repoNames.map((name) => {
+              return (
+                <ListItem dense={true}>
+                  <ListItemIcon>
+                    <BiGitBranch />
+                  </ListItemIcon>
+                  <ListItemText primary={name} />
+                </ListItem>
+              );
+            })}
+          </List>
+        </>
+      )}
     </>
   );
 };

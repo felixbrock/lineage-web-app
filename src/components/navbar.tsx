62639504e4d1346196a08f7e20cefc58c0479a02
felix-brockmeier <felix.brockmeier@outlook.com>
2022-11-21 14:49:02 +0100

    made loading button invisible for now

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -178,7 +178,7 @@ export default function Navbar({
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {snapshotState === 'loading' || snapshotState === 'creating' ? (
-                  <div className="relative ml-3 animate-spin rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <div className="relative ml-3 animate-spin rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 invisible">
                     <RiRefreshLine className="h-6 w-6" aria-hidden="true" />
                   </div>
                 ) : (

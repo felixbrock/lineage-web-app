a4b1038f8e2e65ea434aea9328c29d8c4cc510fd
felix-brockmeier <felix.brockmeier@outlook.com>
2022-11-21 14:39:53 +0100

    made refresh button invisible for now

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -184,14 +184,14 @@ export default function Navbar({
                 ) : (
                   <button
                     type="button"
-                    className="relative ml-3 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    className="relative ml-3 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 invisible"
                     onClick={handleLineageSnapshotRefresh}
                   >
                     <span className="sr-only">Create new snapshot</span>
                     <RiRefreshLine className="h-6 w-6" aria-hidden="true" />
                   </button>
                 )}
-                <i className="relative ml-3 text-xs text-gray-300">
+                <i className="relative ml-3 text-xs text-gray-300 invisible">
                   Latest Snapshot: {snapshotInfo}
                 </i>
                 <button

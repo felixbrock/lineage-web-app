207eedac11ba6ce1aff936414b003375bb7149e2
fbrockm <felix.brockmeier@outlook.com>
2023-02-02 22:12:07 +0100

    minor fixes

--- src/components/integration/snowflake/snowflake.tsx
+++ src/components/integration/snowflake/snowflake.tsx
@@ -5,11 +5,13 @@ import IntegrationApiRepo from '../../../infrastructure/integration-api/integrat
 
 interface SnowflakeProps {
   jwt: string;
-  parentHandleSaveClick: ()=> unknown
+  parentHandleSaveClick: () => unknown;
 }
 
-  
-export default ({ jwt,  parentHandleSaveClick }: SnowflakeProps): ReactElement => {
+export default ({
+  jwt,
+  parentHandleSaveClick,
+}: SnowflakeProps): ReactElement => {
   const [buttonText, setButtonText] = useState('Save');
   const [form, setForm] = useState({
     accountId: '',
@@ -70,7 +72,7 @@ export default ({ jwt,  parentHandleSaveClick }: SnowflakeProps): ReactElement =
                   onChange={(e) => {
                     setForm({
                       ...form,
-                      accountId: e.target.value,
+                      accountId: e.target.value.replaceAll('.', '-'),
                     });
                   }}
                   className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"

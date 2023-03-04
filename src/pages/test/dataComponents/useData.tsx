ab5069f9728ccf628fc41cfe69bfe1063b2cf89d
Philip Bizimis <bizimisphilip@gmail.com>
2023-03-04 18:54:58 +0100

    Add new data loading hooks

--- src/pages/test/dataComponents/useData.tsx
+++ src/pages/test/dataComponents/useData.tsx
@@ -1,55 +1,88 @@
-import { useEffect, useState } from "react";
+import { Auth } from 'aws-amplify';
+import { useEffect, useState } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
+import AccountApiRepository from '../../../infrastructure/account-api/account-api-repo';
 
-function useData(url: any) {
+export function useSessionStorageData(...storageItems: string[]) {
+  let finalStorageObject: { [index: string]: any } = {};
 
-    const respObject = {
-        loading: true,
-        payload: {},
+  for (let item of storageItems) {
+    const sessionStorageItem = sessionStorage.getItem(item);
+    if (!sessionStorageItem) continue;
+    finalStorageObject[item] = JSON.parse(sessionStorageItem);
+  }
+  return finalStorageObject;
+}
+
+export function useApiRepository(
+  jwt: any,
+  apiRepositoryCallback: any,
+  urlSearchParams?: any
+) {
+  const [dtos, setDtos] = useState(undefined);
+
+  useEffect(() => {
+    if (!jwt) return;
+    if (urlSearchParams === undefined) {
+      apiRepositoryCallback(jwt).then((dtos: any) => setDtos(dtos));
+    } else {
+      apiRepositoryCallback(new URLSearchParams({}), jwt).then((dtos: any) =>
+        setDtos(dtos)
+      );
     }
+  }, [jwt]);
 
-    const [data, setData] = useState(respObject);
-
-    useEffect(() => {
-        let ignore = false;
-        fetch(url)
-            .then(response => response.json())
-            .then(json => {
-                if (!ignore) {
-                    setData({ loading: false, payload: json });
-                }
-            });
-        return () => {
-            ignore = true;
-        };
-    }, [url]);
-
-    return data;
+  return dtos;
 }
 
-function useSessionStorageData() {
+export function useAccount() {
+  const location = useLocation();
+  const [searchParams] = useSearchParams();
 
-    const sessionStorageLineage = sessionStorage.getItem('lineage');
-    if (!sessionStorageLineage) return undefined;
+  const [jwt, setJwt] = useState<any>();
+  const [account, setAccount] = useState<any>();
 
-    const sessionStorageMats = sessionStorage.getItem('mats');
-    const sessionStorageCols = sessionStorage.getItem('cols');
+  useEffect(() => {
+    if (!location || !searchParams) return;
 
-    return {
-        lineage: JSON.parse(sessionStorageLineage),
-        mats: sessionStorageMats ? JSON.parse(sessionStorageMats) : [],
-        cols: sessionStorageCols ? JSON.parse(sessionStorageCols) : [],
-    };
-};
+    Auth.currentAuthenticatedUser()
+      .then((cognitoUser) => {
+        // TODO What is the case if no cognitoUser is found?
+        if (!cognitoUser) throw new Error('No Cognito User');
+        Auth.currentSession()
+          .then((session) => {
+            const accessToken = session.getAccessToken();
 
-export function useTestData() {
-    const apiEndpoint = "https://jsonplaceholder.typicode.com/todos/1"
-    const testData = useData(apiEndpoint);
+            let jwtToken = accessToken.getJwtToken();
+            setJwt(jwtToken);
+            return AccountApiRepository.getBy(
+              new URLSearchParams({}),
+              jwtToken
+            );
+          })
+          .then((accounts) => {
+            if (!accounts.length) throw new Error(`No accounts found for user`);
 
-    if (!testData.loading) return buildTestDataFromSource(testData)
+            if (accounts.length > 1)
+              throw new Error(`Multiple accounts found for user`);
 
-    return testData
+            setAccount(accounts[0]);
+          })
+          .catch((error) => {
+            console.trace(typeof error === 'string' ? error : error.message);
+            caches.delete('lineage');
+            sessionStorage.clear();
+
+            Auth.signOut();
+          });
+      })
+      .catch((error) => {
+        console.trace(typeof error === 'string' ? error : error.message);
+
+        Auth.federatedSignIn();
+      });
+  }, [location, searchParams]);
+
+  return [jwt, account];
 }
 
-function buildTestDataFromSource(testData: any) {
-    return { test: true }
-}
--- src/pages/test/newtest.tsx
+++ src/pages/test/newtest.tsx
@@ -1,41 +1,62 @@
-import Navbar from "../../components/navbar";
-import MainTable from "./tableComponents/mainTable";
-import SearchBox from "../lineage/components/search-box";
-import { useTestData } from "./dataComponents/useData";
+import Navbar from '../../components/navbar';
+import MainTable from './tableComponents/mainTable';
+import SearchBox from '../lineage/components/search-box';
+import { useAccount, useApiRepository } from './dataComponents/useData';
+import MaterializationsApiRepository from '../../infrastructure/lineage-api/materializations/materializations-api-repository';
+import ColumnsApiRepository from '../../infrastructure/lineage-api/columns/columns-api-repository';
+import ObservabilityApiRepo from '../../infrastructure/observability-api/observability-api-repo';
+import { useEffect, useState } from 'react';
+import { buildTestSelectionStructure } from './dataComponents/buildTestData';
 
 export default function NewTest() {
+  const [jwt, account] = useAccount();
+  const mats = useApiRepository(jwt, MaterializationsApiRepository.getBy, {});
+  const cols = useApiRepository(jwt, ColumnsApiRepository.getBy, {});
+  const testSuite = useApiRepository(jwt, ObservabilityApiRepo.getTestSuites);
+  const testQualSuite = useApiRepository(
+    jwt,
+    ObservabilityApiRepo.getQualTestSuites
+  );
 
-    const testData = useTestData()
+  const [tableData, setTableData] = useState({ loading: true, tableData: {} });
 
-    console.log(testData)
+  useEffect(() => {
+    if (mats && cols && testSuite && testQualSuite) {
+      setTableData(
+        buildTestSelectionStructure(mats, cols, testSuite, testQualSuite)
+      );
+    }
+  }, [mats, cols, testSuite, testQualSuite]);
 
-    return (
-            <div className="h-screen w-full">
-                <Navbar current="tests" jwt={undefined} />
-                <div className="items-top relative flex h-20 justify-center">
-                    <div className="relative mt-2 w-1/4">
-                        <SearchBox
-                            placeholder="Search..."
-                            label="testsearchbox"
-                            onChange={() => { }}
-                        />
-                    </div>
-                </div>
-                <MainTable
-                    buttonOnClick={() => { }}
-                    buttonText={"Columns"}
-                    buttonIsDisclosure={true}
-                    buttonDisclosureContent={
-                        <MainTable
-                            buttonOnClick={() => { }}
-                            buttonText={"Edit"}
-                            buttonIsDisclosure={false}
-                            darkMode={true}
-                        />
-                    }
-                    tableTitle="Tables"
-                    tableDescription="This is a test description."
-                />
-            </div>
-    );
+  console.log(tableData);
+
+  return (
+    <div className="h-screen w-full">
+      <Navbar current="tests" jwt={undefined} />
+      <div className="items-top relative flex h-20 justify-center">
+        <div className="relative mt-2 w-1/4">
+          <SearchBox
+            placeholder="Search..."
+            label="testsearchbox"
+            onChange={() => {}}
+          />
+        </div>
+      </div>
+      <MainTable
+        buttonOnClick={() => {}}
+        buttonText={'Columns'}
+        buttonIsDisclosure={true}
+        buttonDisclosureContent={
+          <MainTable
+            buttonOnClick={() => {}}
+            buttonText={'Edit'}
+            buttonIsDisclosure={false}
+            darkMode={true}
+          />
+        }
+        tableTitle="Tables"
+        tableDescription="This is a test description."
+      />
+    </div>
+  );
 }

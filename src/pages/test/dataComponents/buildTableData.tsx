de1108d29273df2901147dbf59504aadd83da6a7
Philip Bizimis <bizimisphilip@gmail.com>
2023-03-05 19:21:57 +0100

    Add first mapping step of raw data

--- src/pages/test/dataComponents/buildTableData.tsx
+++ src/pages/test/dataComponents/buildTableData.tsx
@@ -38,14 +38,42 @@ interface Database {
 type TableData = Map<string, Database>;
 
 export function buildTableData(
-  mats: MaterializationDto,
-  cols: ColumnDto,
-  testSuites: TestSuiteDto,
-  qualTestSuites: QualTestSuiteDto
+  mats: MaterializationDto[],
+  cols: ColumnDto[],
+  testSuites: TestSuiteDto[],
+  qualTestSuites: QualTestSuiteDto[]
 ): TableData {
   const tableData: TableData = new Map();
 
+  console.log(mats);
 
+  for (let mat of mats) {
+    const databaseName = mat.databaseName;
+    const schemaName = mat.schemaName;
 
+    const database = tableData.get(databaseName);
+
+    if (database) {
+      const schema = database.schemas.get(schemaName);
+      if (schema) {
+        // @ts-ignore
+        schema.tables.set(mat.name, mat);
+      } else {
+        database.schemas.set(schemaName, {
+          // @ts-ignore
+          tables: new Map([[mat.name, mat]]),
+        });
+      }
+    } else {
+      tableData.set(databaseName, {
+    // @ts-ignore
+        schemas: new Map([
+          [schemaName, { tables: new Map([[mat.name, mat]]) }],
+        ]),
+      });
+    }
+  }
+
+  console.log(tableData);
   return tableData;
 }

5fe99a2742e5779477914b6c9d4a54fd5d988d8c
Philip Bizimis <bizimisphilip@gmail.com>
2023-03-06 21:38:17 +0100

    Add creation of column and table test maps

--- src/pages/test/dataComponents/buildTableData.tsx
+++ src/pages/test/dataComponents/buildTableData.tsx
@@ -14,13 +14,15 @@ interface Test {
 
 interface Column {
   tests: Test[];
+  name: string;
 }
 
 type Columns = Map<string, Column>;
 
 interface Table {
-  columns: Columns;
   tests: Test[];
+  name: string;
+  columns?: Columns;
 }
 
 type Tables = Map<string, Table>;
@@ -37,6 +39,8 @@ interface Database {
 
 type TableData = Map<string, Database>;
 
+// Optimize by returning all objects as maps with id as key
+
 export function buildTableData(
   mats: MaterializationDto[],
   cols: ColumnDto[],
@@ -46,6 +50,8 @@ export function buildTableData(
   const tableData: TableData = new Map();
 
   console.log(mats);
+  console.log(testSuites);
+  console.log(qualTestSuites);
 
   for (let mat of mats) {
     const databaseName = mat.databaseName;
@@ -53,27 +59,78 @@ export function buildTableData(
 
     const database = tableData.get(databaseName);
 
+    const tableObject: Table = {
+      columns: new Map(),
+      tests: [],
+      name: '',
+    };
+
+    const schemaObject: Schema = {
+      tables: new Map([[mat.id, tableObject]]),
+    };
+
     if (database) {
       const schema = database.schemas.get(schemaName);
       if (schema) {
-        // @ts-ignore
-        schema.tables.set(mat.name, mat);
+        schema.tables.set(mat.id, tableObject);
       } else {
-        database.schemas.set(schemaName, {
-          // @ts-ignore
-          tables: new Map([[mat.name, mat]]),
-        });
+        database.schemas.set(schemaName, schemaObject);
       }
     } else {
       tableData.set(databaseName, {
-    // @ts-ignore
-        schemas: new Map([
-          [schemaName, { tables: new Map([[mat.name, mat]]) }],
-        ]),
+        schemas: new Map([[schemaName, schemaObject]]),
       });
     }
   }
 
+  function mapTests() {
+    const columns: Columns = new Map();
+    const tables: Tables = new Map();
+
+    function addTestToMap(
+      parentMap: Columns | Tables,
+      parentMapValue: Table | Column,
+      parentMapKey: string,
+      testInfo: Test
+    ) {
+      const testParent = parentMap.get(parentMapKey);
+      if (testParent) {
+        testParent.tests.push(testInfo);
+      } else {
+        parentMap.set(parentMapKey, parentMapValue);
+      }
+    }
+
+    for (let test of [...testSuites, ...qualTestSuites]) {
+      const testTargetId = test.target.targetResourceId;
+
+      const testInfo: Test = {
+        name: test.type,
+        cron: test.cron,
+        active: test.activated,
+        threshold: '',
+      };
+
+      if (test.target.columnName) {
+        const column: Column = {
+          tests: [testInfo],
+          name: '',
+        };
+        addTestToMap(columns, column, testTargetId, testInfo);
+      } else {
+        const table: Table = {
+          tests: [testInfo],
+          name: '',
+          columns: new Map(),
+        };
+        addTestToMap(tables, table, testTargetId, testInfo);
+      }
+    }
+    return [columns, tables]
+  }
+  const [columns,tables] = mapTests()
+
+    console.log(columns, tables)
   console.log(tableData);
   return tableData;
 }

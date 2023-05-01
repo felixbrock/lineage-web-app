00df63ff6449e7528818d84bd036bc6a51881a2e
Philip Bizimis <bizimisphilip@gmail.com>
2023-05-01 18:33:18 +0200

    Fix lint errors

--- src/pages/test/test.tsx
+++ src/pages/test/test.tsx
@@ -1,4 +1,6 @@
 import Navbar from '../../components/navbar';
+import { Paginator } from './tableComponents/pagination';
+import { Alert, AlertInfo } from './tableComponents/alert';
 import { NewTestState } from './tableComponents/mainTable';
 import SearchBox from '../lineage/components/search-box';
 import { useAccount, useApiRepository } from './dataComponents/useData';
@@ -12,7 +14,6 @@ import {
   Schema,
   TableData,
 } from './dataComponents/buildTableData';
-import { Transition } from '@headlessui/react';
 import { changeTests } from './dataComponents/handleTestData';
 import { Theme, colorConfig, GlobalTableColorConfig, Level } from './config';
 import MaterializationDto from '../../infrastructure/lineage-api/materializations/materialization-dto';
@@ -23,11 +24,6 @@ import {
 } from '../../infrastructure/observability-api/test-suite-dto';
 import { TestType } from './dataComponents/buildTableData';
 
-export interface AlertInfo {
-  show: boolean;
-  title: string;
-  description: string;
-}
 
 export interface CurrentTestStates {
   tests: [TestSuiteDto[], React.Dispatch<React.SetStateAction<TestSuiteDto[]>>];
@@ -65,16 +61,16 @@ const tableContext: TableContextProps = {
 export const TableContext = createContext(tableContext);
 
 export default function Test() {
-  const [jwt, account] = useAccount();
+  const jwt = useAccount();
   const [currentTheme, setCurrentTheme] = useState(
     tableContext.theme.currentTheme
   );
-  const [mats, setMats] = useApiRepository(
+  const mats = useApiRepository(
     jwt,
     MaterializationsApiRepository.getBy,
     {}
-  );
-  const [cols, setCols] = useApiRepository(jwt, ColumnsApiRepository.getBy, {});
+  )[0];
+  const cols = useApiRepository(jwt, ColumnsApiRepository.getBy, {})[0];
   const [testSuite, setTestSuite] = useApiRepository(
     jwt,
     ObservabilityApiRepo.getTestSuites
@@ -105,6 +101,45 @@ export default function Test() {
     description: '',
   };
   const [alertInfo, setAlertInfo] = useState(alertInfoInit);
+
+  function searchTableData(tableData: TableData, searchString: string) {
+    const searchResults: TableData = new Map();
+
+    tableData.forEach((database, databaseName) => {
+      database.schemas.forEach((schema, schemaName) => {
+        schema.tables.forEach((table, tableId) => {
+          if (
+            !table.name.includes(searchString) &&
+            !schemaName.includes(searchString) &&
+            !databaseName.includes(searchString)
+          )
+            return;
+          const currentDb = searchResults.get(databaseName);
+          if (currentDb) {
+            const currentSchema = currentDb.schemas.get(schemaName);
+            if (currentSchema) {
+              currentSchema.tables.set(tableId, table);
+            } else {
+              const newSchema: Schema = { tables: new Map([[tableId, table]]) };
+              currentDb.schemas.set(schemaName, newSchema);
+            }
+          } else {
+            const newSchema: Schema = { tables: new Map([[tableId, table]]) };
+            const newDatabase: Database = {
+              schemas: new Map([[schemaName, newSchema]]),
+            };
+            searchResults.set(databaseName, newDatabase);
+          }
+        });
+      });
+    });
+
+    console.log(searchResults);
+    if (searchResults.size === 0) return tableData;
+    return searchResults;
+  }
+
+  const [searchString, setSearchString] = useState('');
 
   useEffect(() => {
     if (mats && cols && testSuite && testQualSuite) {
@@ -154,44 +189,8 @@ export default function Test() {
 
   console.log(tableState);
 
-  function searchTableData(tableData: TableData, searchString: string) {
-    const searchResults: TableData = new Map();
 
-    tableData.forEach((database, databaseName) => {
-      database.schemas.forEach((schema, schemaName) => {
-        schema.tables.forEach((table, tableId) => {
-          if (
-            !table.name.includes(searchString) &&
-            !schemaName.includes(searchString) &&
-            !databaseName.includes(searchString)
-          )
-            return;
-          const currentDb = searchResults.get(databaseName);
-          if (currentDb) {
-            const currentSchema = currentDb.schemas.get(schemaName);
-            if (currentSchema) {
-              currentSchema.tables.set(tableId, table);
-            } else {
-              const newSchema: Schema = { tables: new Map([[tableId, table]]) };
-              currentDb.schemas.set(schemaName, newSchema);
-            }
-          } else {
-            const newSchema: Schema = { tables: new Map([[tableId, table]]) };
-            const newDatabase: Database = {
-              schemas: new Map([[schemaName, newSchema]]),
-            };
-            searchResults.set(databaseName, newDatabase);
-          }
-        });
-      });
-    });
 
-    console.log(searchResults);
-    if (searchResults.size === 0) return tableData;
-    return searchResults;
-  }
-
-  const [searchString, setSearchString] = useState('');
 
   useEffect(() => {
     setTableState({
@@ -238,67 +237,5 @@ export default function Test() {
         )}
       </div>
     </TableContext.Provider>
-  );
-}
-
-import { XMarkIcon } from '@heroicons/react/20/solid';
-import { Paginator } from './tableComponents/pagination';
-
-function Alert({
-  alertInfo,
-  setAlertInfo,
-}: {
-  alertInfo: AlertInfo;
-  setAlertInfo: React.Dispatch<React.SetStateAction<AlertInfo>>;
-}) {
-  return (
-    <>
-      {/* Global notification live region, render this permanently at the end of the document */}
-      <div
-        aria-live="assertive"
-        className="pointer-events-none fixed inset-0 top-16 z-50 flex items-end px-4 py-6 sm:items-start sm:p-6"
-      >
-        <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
-          {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
-          <Transition
-            show={alertInfo.show}
-            as={Fragment}
-            enter="transform ease-out duration-300 transition"
-            enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-            enterTo="translate-y-0 opacity-100 sm:translate-x-0"
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-cito shadow-lg ring-1 ring-black ring-opacity-5">
-              <div className="p-4">
-                <div className="flex items-start">
-                  <div className="ml-3 w-0 flex-1 pt-0.5">
-                    <p className="text-sm font-medium text-white">
-                      {alertInfo.title}
-                    </p>
-                    <p className="mt-1 text-sm text-white">
-                      {alertInfo.description}
-                    </p>
-                  </div>
-                  <div className="ml-4 flex flex-shrink-0">
-                    <button
-                      type="button"
-                      className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      onClick={() => {
-                        setAlertInfo({ ...alertInfo, show: false });
-                      }}
-                    >
-                      <span className="sr-only">Close</span>
-                      <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Transition>
-        </div>
-      </div>
-    </>
   );
 }

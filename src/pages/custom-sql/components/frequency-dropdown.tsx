70c8c0b8546ccf34e97fbd777ad14fb8cf954827
aah120 <aah120@ic.ac.uk>
2023-06-05 15:44:27 +0100

    create frequency dropdown component and finish front end implementation

---
+++ src/pages/custom-sql/components/frequency-dropdown.tsx
@@ -0,0 +1,106 @@
+import React, { Fragment } from "react";
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
+
+export default function FrequencyDropdown({
+    newTestFrequency,
+    setNewTestFrequency
+}: {
+    newTestFrequency: string,
+    setNewTestFrequency: React.Dispatch<React.SetStateAction<string>>
+}) {
+
+    const frequencies = ['1h', '3h', '6h', '12h', '24h'];
+
+    const handleChange = (event: string) => {
+        setNewTestFrequency(event);
+    }
+   
+    return (
+      <Listbox
+        value={newTestFrequency}
+        onChange={handleChange}
+      >
+        {({ open }) => (
+          <>
+            <div className="relative">
+              <Listbox.Button 
+                className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm 
+                                            focus:border-cito focus:outline-none focus:ring-1 focus:ring-cito sm:text-sm"
+                >
+                <span className="block truncate">
+                    {newTestFrequency ? newTestFrequency : <span className="text-gray-500">Select Frequency...</span>}
+                </span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                  <ChevronUpDownIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </Listbox.Button>
+  
+              <Transition
+                show={open}
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute z-40 mt-1 w-full min-w-[7rem] overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 
+                                                ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {Object.values(frequencies).map((frequencyOption) => {
+                    return (
+                      <Listbox.Option
+                        key={frequencyOption}
+                        className={({ active }) =>
+                            `${active ? 'bg-cito text-white' : 'text-gray-900'} relative cursor-default select-none py-2 pl-3 pr-9`
+                        }
+                        value={frequencyOption}
+                      >
+                        {({ selected, active }) => (
+                          <>
+                            <span
+                              className={`${selected ? 'font-semibold' : 'font-normal'} block truncate`}
+                            >
+                            {frequencyOption}
+                            </span>
+  
+                            {selected ? (
+                              <span
+                                className={`${active ? 'text-white' : 'text-cito'} absolute inset-y-0 right-0 flex items-center pr-4`}
+                              >
+                                <CheckIcon
+                                  className="h-5 w-5"
+                                  aria-hidden="true"
+                                />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    );
+                  })}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </>
+        )}
+      </Listbox>
+    );
+  }
+
+  {/* <select 
+    className="block w-full px-4 py-2 cursor-default absolute z-40 mt-1 w-full min-w-[7rem] overflow-auto rounded-md bg-white text-base 
+                border border-gray-200 bg-gray-200 focus:border-gray-500 focus:bg-white focus:outline-none focus:border-cito transition 
+                duration-300 focus:ring-1 focus:ring-cito sm:text-sm"
+    required
+    value={newTestFrequency}
+    onChange={(e) => setNewTestFrequency(e.target.value)}
+  >
+    <option value="">Select Frequency...</option>
+    <option value="1h">1h...</option>
+    <option value="3h">3h...</option>
+    <option value="6h">6h...</option>
+    <option value="12h">12h...</option>
+    <option value="24h">24h...</option>
+  </select> */}
--- src/pages/custom-sql/custom-sql.tsx
+++ src/pages/custom-sql/custom-sql.tsx
@@ -2,12 +2,15 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { useAccount } from "../test/dataComponents/useData";
 import Navbar from "../../components/navbar";
 import { Alert, AlertInfo } from "../test/tableComponents/alert";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import SearchBox from "../lineage/components/search-box";
 import ObservabilityApiRepo from "../../infrastructure/observability-api/observability-api-repo";
 import { CustomTestSuiteDto } from "../../infrastructure/observability-api/test-suite-dto";
 import Toggle from "../custom-sql/components/toggle";
 import { getFrequency } from "../test/utils/cron";
+import { Transition } from "@headlessui/react";
+import { MdClose } from "react-icons/md";
+import FrequencyDropdown from "./components/frequency-dropdown";
 
 
 export default function CustomSql() {
@@ -16,6 +19,14 @@ export default function CustomSql() {
     const [tests, setTests] = useState<CustomTestSuiteDto[]>([]);
     const [searchString, setSearchString] = useState('');
     const [searchResults, setSearchResults] = useState<CustomTestSuiteDto[]>([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const [newTestName, setNewTestName] = useState('');
+    const [newTestDescription, setNewTestDescription] = useState('');
+    const [newTestSql, setNewTestSql] = useState('');
+    const [newTestFrequency, setNewTestFrequency] = useState('');
+    const [newTestLowerThreshold, setNewTestLowerThreshold] = useState('');
+    const [newTestUpperThreshold, setNewTestUpperThreshold] = useState('');
     
     const alertInfoInit: AlertInfo = {
         show: false,
@@ -54,6 +65,25 @@ export default function CustomSql() {
         setSearchResults(results);
     };
 
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+
+    const handleCancelButton = () => {
+        setNewTestName('');
+        setNewTestDescription('');
+        setNewTestSql('');
+        setNewTestFrequency('');
+        setNewTestLowerThreshold('');
+        setNewTestUpperThreshold('');
+        
+        toggleModal();
+    };
+
+    const handleSubmitButton = () => {
+
+    };
+
     useEffect(() => {
         renderTests();
     }, []);
@@ -68,7 +98,7 @@ export default function CustomSql() {
                 <Navbar current="custom-sql" jwt={jwt} />
                 <Alert alertInfo={alertInfo} setAlertInfo={setAlertInfo} />
                 <div className="items-center flex h-20 justify-center">
-                    <div className="relative flex items-center mt-2 w-1/4">
+                    <div className="relative flex items-center w-1/4">
                         <SearchBox
                         placeholder="Search..."
                         label="testsearchbox"
@@ -76,12 +106,16 @@ export default function CustomSql() {
                         />
                     </div>
                     <div className="absolute right-0 p-6">
-                            <button className="border-2 border-purple-600 text-purple-600 hover:bg-purple-600 hover:text-white px-4 py-2 rounded">Create New Test</button>
+                            <button 
+                                className="border-2 border-purple-600 text-purple-600 hover:bg-purple-600 hover:text-white transition-all duration-500 px-4 py-2 rounded"
+                                onClick={toggleModal}>
+                                Create New Test
+                            </button>
                     </div>
                 </div>
                 <div className="p-6">
                     {searchResults.map((test, index) => (
-                        <div key={index} className="p-4 shadow-lg mb-6">
+                        <div key={index} className="p-4 shadow-2xl mb-6 border-2 hover:border-purple-600 transition-all duration-500 rounded-lg">
                             <div className="flex items-center">
                                 <h1 className="flex-grow"><span className="font-bold">Test Name: </span>{test.name}</h1>
                                 <Toggle test={test} />
@@ -98,6 +132,116 @@ export default function CustomSql() {
                     }
                 </div>
             </div>
+            <Transition appear show={isModalOpen} className="relative">
+                <>
+                    <div className="fixed inset-0 z-60 bg-black opacity-50"></div>
+
+                    <Transition.Child
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="transition ease-in duration-150"
+                        leaveFrom='opacity-100 scale-100'
+                        leaveTo='opacity-0 scale-95'
+                    >
+
+                        <div className="fixed inset-x-0 z-60 mx-auto w-3/4 max-w-3xl flex flex-col bg-white rounded-2xl p-6 transition-all" style={{top: "10%"}}>
+                            <div className="flex justify-between mb-1">
+                                <h3 className="text-2xl mt-2 font-bold leading-6 text-gray-900">Create New Custom Test</h3>
+                                <button type="button" className="flex items-center justify-end px-4 py-2" onClick={handleCancelButton}>
+                                    <MdClose className="h-6 w-6 fill-gray-500 hover:fill-cito" />
+                                </button>
+                            </div>
+                            <p className="text-right text-sm text-red-500 mt-2 mb-4">* Indicates required field</p>
+                            
+                            <div className="flex items-center mb-2">
+                                <h2 className="mr-2">Name<span className="text-red-500">*</span></h2>
+                            </div>
+                            <input
+                                className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 p-2 leading-tight text-gray-700 
+                                            focus:border-gray-500 focus:bg-white focus:outline-none transition duration-300"
+                                required
+                                type="text"
+                                placeholder="Enter the name for your test..."
+                                value={newTestName}
+                                onChange={(e) => setNewTestName(e.target.value)}
+                            />
+                            
+                            <div className="mt-4 flex items-center mb-2">
+                                <h2 className="mr-2">Description</h2>
+                            </div>
+                            <input
+                                className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 p-2 leading-tight text-gray-700 
+                                            focus:border-gray-500 focus:bg-white focus:outline-none transition duration-300"
+                                type="text"
+                                placeholder="Enter the description for your test..."
+                                value={newTestDescription}
+                                onChange={(e) => setNewTestDescription(e.target.value)}
+                            />
+                            
+                            <div className="mt-4 flex items-center mb-2">
+                                <h2 className="mr-2">SQL Logic<span className="text-red-500">*</span></h2>
+                            </div>
+                            <textarea
+                                rows={2}
+                                className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 p-2 leading-tight text-gray-700 
+                                            focus:border-gray-500 focus:bg-white focus:outline-none transition duration-300"
+                                required
+                                placeholder="Enter the SQL logic for your test..."
+                                value={newTestSql}
+                                onChange={(e) => setNewTestSql(e.target.value)}
+                            />
+
+                            <div className="mt-4 flex items-center mb-2">
+                                <h2 className="mr-2">Frequency<span className="text-red-500">*</span></h2>                                
+                            </div>
+                            <FrequencyDropdown newTestFrequency={newTestFrequency} setNewTestFrequency={setNewTestFrequency} />
+                            <div className="mt-4 flex items-center mb-2">
+                                <h2 className="mr-2">Lower Threshold</h2>
+                            </div>
+                            <input
+                                className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 p-2 leading-tight text-gray-700 
+                                            focus:border-gray-500 focus:bg-white focus:outline-none transition duration-300"
+                                type="number"
+                                placeholder="Enter the lower threshold for your test..."
+                                value={newTestLowerThreshold}
+                                onChange={(e) => setNewTestLowerThreshold(e.target.value)}
+                            />
+
+                            <div className="mt-4 flex items-center mb-2">
+                                <h2 className="mr-2">Upper Threshold</h2>
+                            </div>
+                            <input
+                                className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 p-2 leading-tight text-gray-700 
+                                            focus:border-gray-500 focus:bg-white focus:outline-none transition duration-300"
+                                type="number"
+                                placeholder="Enter the upper threshold for your test..."
+                                value={newTestUpperThreshold}
+                                onChange={(e) => setNewTestUpperThreshold(e.target.value)}
+                            />
+
+                            <div className="flex flex-row mt-4 justify-end">
+                                <button 
+                                    className="border-2 border-purple-600 text-purple-600 hover:bg-purple-600 hover:text-white transition-all 
+                                                duration-500 p-2 mb-2 mr-4 rounded"
+                                    onClick={toggleModal}
+                                >
+                                    Submit
+                                </button>
+                                <button 
+                                    className="border-2 border-purple-600 text-purple-600 hover:bg-purple-600 hover:text-white transition-all 
+                                                duration-500 p-2 mb-2 rounded"
+                                    onClick={handleCancelButton}
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </div>
+
+                    </Transition.Child>
+                </>
+            </Transition>
         </ThemeProvider>
 
     );
